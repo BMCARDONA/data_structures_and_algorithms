7e9013ca816ea9b5646c86f86cdb3c10a925177c
Bradley Cardona <97191141+BMCARDONA@users.noreply.github.com>
2023-05-17 18:39:26 +0000

    Sync LeetCode submission - Contains Duplicate (python)

---
+++ solutions/problems/contains_duplicate/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        # Time: O(n)
+        # Space: O(n)
+        hashSet = set()
+        for n in nums:
+            if n in hashSet:
+                return True
+            hashSet.add(n)
+        return False
+
