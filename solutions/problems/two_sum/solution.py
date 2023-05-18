a4319004225e336b3de538b71a40da79771e987c
Bradley Cardona <97191141+BMCARDONA@users.noreply.github.com>
2023-05-18 00:22:50 +0000

    Sync LeetCode submission - Two Sum (python3)

--- solutions/problems/two_sum/solution.py
+++ solutions/problems/two_sum/solution.py
@@ -1,7 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i != j and nums[i] + nums[j] == target:
-                    return [i, j]
+        hashMap = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in hashMap:
+                return [i, hashMap[diff]]
+            hashMap[num] = i
     
